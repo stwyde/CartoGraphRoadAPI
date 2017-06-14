8196c022dddd0e1107e7f82f73567f8ffcf61cd7
Eloa Franca Verona <efrancav@macalester.edu>
2017-06-14 10:03:48 -0500

    changed list from dictionaries

--- src/RoadsAPI.py
+++ src/RoadsAPI.py
@@ -1,29 +1,55 @@
-import csv
+from collections import defaultdict
+
+
 class RoadLoader:
 
     def __init__(self, pathToBundledVertices = './DataFiles/output_verticesWiki.txt',
                  pathToEdges = './DataFiles/output_edgesWiki.txt',
-                 pathToOriginalVertices = './DataFiles/Original Vertices.txt'):
-        self.bundledVertices= []
+                 pathToOriginalVertices = './DataFiles/Original Vertices.txt',
+                 pathToOriginalEdges = './DataFiles/OriginalEdges.txt'):
+
+        self.bundledVertices= {}
         with open(pathToBundledVertices) as ptbv: #this seem like bad way to do this
             for line in ptbv:
-                self.bundledVertices.append(line.split())
-        self.bundledEdges = []
+                lst = line.split()
+                self.bundledVertices[lst[0]] = lst[1:]
+
+        self.bundledEdges = defaultdict(list)
         with open(pathToEdges) as pte:
             for line in pte:
-                self.bundledEdges.append(line.split())
-        self.originalVertices = []
+                lst = line.split()
+                self.bundledEdges[lst[0]].append(lst[1:])
+
+        self.originalVertices = {}
         with open(pathToOriginalVertices) as ptov:
             for line in ptov:
                 lst  = line.split()
                 if len(lst) == 1: continue
-                self.originalVertices.append(lst)
+                self.originalVertices[lst[0]] = lst[1:]
 
+        self.originalEdges = defaultdict(list)
+        with open(pathToOriginalEdges) as ptoe:
+            for line in ptoe:
+                lst = line.split()
+                if len(lst) == 1: continue
+                self.originalEdges[lst[0]].append(lst[1:])
 
 
     def get_vertices_and_edges_at_viewport(self, view_port = [0, 0, 0, 0]):
 
         pass
 
+
+    def verticeIsInViewPort(self, edgeCoor = [0, 0], view_port = [0, 0, 0, 0]):
+        return True
+
+
+
 cTests = RoadLoader()
-print(cTests.originalVertices[:10])
+ov = cTests.originalVertices.keys()
+bv = cTests.bundledVertices.keys()
+
+oe = cTests.originalEdges.keys()
+be = cTests.bundledEdges.keys()
+
+print(cTests.originalVertices[ov[11]], cTests.bundledVertices[bv[11]], cTests.originalEdges[oe[11]], cTests.bundledEdges[be[11]])
