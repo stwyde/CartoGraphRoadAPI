5abf5eb807265cc68ce47f78b465538222508e48
Talha Ahsan <stwyde@gmail.com>
2017-06-16 14:56:41 -0500

    TODO: Fix edge ID uniqueness

--- src/TestingGrounds.py
+++ src/TestingGrounds.py
@@ -5,40 +5,40 @@ newEdges = open("../simpleData/output_edgesWiki.txt", "r")
 semanticTree = open("../simpleData/output_semanticTreeWiki.txt", "r")
 
 def hunt(childEdgeId, frontStack, backStack, childEdges, topParents):
-    for line in childEdges:
-        if childEdgeId in line[0]:
-            frontStack.append(line[1])
-            backStack.insert(0, line[2])
-            return hunt(line[3], frontStack, backStack, childEdges, topParents)
-    for line in topParents:
-        if childEdgeId in line[0]:
-            frontStack.append(line[1])
-            backStack.insert(0, line[2])
-            #merges front and back stacks
-            for node in backStack:
-                frontStack.append(node)
-            return frontStack
+    if childEdgeId in childEdges:
+        frontStack.append(childEdges[childEdgeId][0])
+        backStack.insert(0, childEdges[childEdgeId][1])
+        newChildEdge = childEdges[childEdgeId][2]
+        print(newChildEdge)
+        return hunt(newChildEdge, frontStack, backStack, childEdges, topParents)
+
+    elif childEdgeId in topParents:
+        frontStack.append(topParents[childEdgeId][0])
+        backStack.insert(0,topParents[childEdgeId][1])
+        #merges front and back stacks
+        for node in backStack:
+            frontStack.append(node)
+        return frontStack
 
 def hunt2(childEdgeId, frontStack, backStack, childEdges, topParents):
     foundParent = False
     while(foundParent == False):
-        haschildParent = False
-        for line in childEdges:
-            if childEdgeId in line[0]:
-                frontStack.append(line[1])
-                backStack.insert(0, line[2])
-                childEdgeId = line[3]
-                haschildParent = True
-                break
-        if(haschildParent == False):
-            for line in topParents:
-                if childEdgeId in line[0]:
-                    frontStack.append(line[1])
-                    backStack.insert(0, line[2])
-                    #merges front and back stacks
-                    for node in backStack:
-                        frontStack.append(node)
-                    return frontStack
+        if(childEdgeId in childEdges):
+            line = childEdges[childEdgeId]
+            frontStack.append(line[0])
+            backStack.insert(0, line[1])
+            childEdgeId = line[3]
+            print(frontStack)
+
+        elif(childEdgeId in topParents):
+            line = topParents[childEdgeId]
+            frontStack.append(line[0])
+            backStack.insert(0, line[1])
+            #merges front and back stacks
+            for node in backStack:
+                frontStack.append(node)
+            foundParent = True
+            return frontStack
 
 origVert = [x.rstrip() for x in origVert]
 origEdges = [x.rstrip() for x in origEdges]
@@ -61,29 +61,46 @@ for line in origEdges:
     a,b = line.split(" ")
     pairings.append((a,b))
 
-#Now work on pathing!
-topParents = []
-childEdges = []
+#Now work on pathing! Commented version implements array of lines, current version uses a dict for semantic tree
+# topParents = []
+# childEdges = []
+# for line in semanticTree:
+#      line2 = line
+#      splitLine = line2.split(" ")
+#      if len(splitLine) == 4:
+#          childEdges.append(splitLine)
+#      elif len(splitLine) == 3:
+#          topParents.append(splitLine)
+#      else:
+#          print("ERROR, SEMANTIC TREE CONTAINS IMPROPERLY FORMATTED OBJECT")
+topParents = {}
+childEdges = {}
 for line in semanticTree:
      line2 = line
      splitLine = line2.split(" ")
      if len(splitLine) == 4:
-         childEdges.append(splitLine)
+         childEdges[splitLine[0]] = splitLine[1:] #Edge ID as key, nodes src dest and parent Edge as vals
      elif len(splitLine) == 3:
-         topParents.append(splitLine)
+         topParents[splitLine[0]] = splitLine[1:] #Edge ID as key, nodes src and dest as vals
      else:
          print("ERROR, SEMANTIC TREE CONTAINS IMPROPERLY FORMATTED OBJECT")
+
 print("Done setting up stuff, now pairing and pathing")
+
 paths = {}
-for pairing in pairings:
-    frontStack = [pairing[0]]
-    backStack = [pairing[1]]
-    lastEdge = "hehe xD"
-    for entry in childEdges:
-        if pairing[0] in entry[1] and pairing[1] in entry[2]:
-            #removes line to make future pathing easier. This only happens for the initial unbundled edge so it should be okay, especially since the relationship is secured in inputEdges
-            childEdges.remove(entry)
-            finalPath = hunt2(entry[3], frontStack, backStack, childEdges, topParents)
-            paths[pairing] = finalPath
-            print(paths[pairing])
-            break
+#for pairing in pairings:
+pairing = pairings.pop()
+frontStack = [pairing[0]]
+backStack = [pairing[1]]
+lastEdge = "hehe xD" #what's this for again?
+for entry in childEdges:
+    if pairing[0] in childEdges[entry][0] and pairing[1] in childEdges[entry][1]:
+        print(lastEdge)
+        #removes line to make future pathing easier. This only happens for the initial unbundled edge so it should be okay, especially since the relationship is secured in inputEdges
+        #childEdges.remove(entry)
+        print entry
+        print childEdges[entry][2]
+        finalPath = hunt(childEdges[entry][2], frontStack, backStack, childEdges, topParents)
+        paths[pairing] = finalPath
+        print(paths[pairing])
+        break
