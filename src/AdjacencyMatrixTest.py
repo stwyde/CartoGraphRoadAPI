9d251eaf876c6c2e0597194382893e52e81806f1
Talha Ahsan <stwyde@gmail.com>
2017-06-20 14:07:01 -0500

    fixed counter vales

--- src/AdjacencyMatrixTest.py
+++ src/AdjacencyMatrixTest.py
@@ -1,17 +1,21 @@
+import datetime
 origVert = open("../simpleData/Original Vertices.txt", "r")
 semanticTree = open("../simpleData/NewSemanticTree.txt", "r")
 
 
 def getPath(childEdgeId, edgeDict, frontStack = [], backStack = []):
+    frontStack.append(edgeDict[childEdgeId][0])
+    backStack.insert(0, edgeDict[childEdgeId][1])
     if(len(edgeDict[childEdgeId]) == 4):
         #we still have parents!
         parentID = edgeDict[childEdgeId][2]
-        frontStack.append(edgeDict[parentID][0])
-        backStack.insert(0, edgeDict[parentID][1])
+        #frontStack.append(edgeDict[parentID][0])
+        #backStack.insert(0, edgeDict[parentID][1])
         return getPath(parentID, edgeDict, frontStack, backStack)
     elif(len(edgeDict[childEdgeId]) == 3):
         #merge stacks:
-        frontStack.append(backStack)
+        for val in backStack:
+            frontStack.append(val)
         return frontStack
 
 def getViewPortPaths(xmin, xmax, ymin, ymax, vertices, outboundPaths, inboundPaths, edgeDict):
@@ -38,14 +42,15 @@ def getViewPortPaths(xmin, xmax, ymin, ymax, vertices, outboundPaths, inboundPat
     print("Finding paths now. Paths to do: " + str(len(inpathsToMine) +len(outpathsToMine)))
     for path in inpathsToMine: #path[0] = src, path[1] = dest
         paths.append(getPath(inboundPaths[path[1]][path[0]][0], edgeDict,[path[1]], [path[0]]))
-        if(len(paths) % 100 == 0):
+        if(len(paths) % 100000 == 0):
             print(len(paths))
     for path in outpathsToMine:
-        paths.append(getPath(outboundPaths[path[0]][path[1]][0], edgeDict, [path[0]], [path[1]]))
-        if (len(paths) % 100 == 0):
+        paths.append(getPath(outboundPaths[path[0]][path[1]][0], edgeDict))
+        if len(paths) % 100000 == 0:
             print(len(paths))
     return paths
 
+oldTime = datetime.datetime.now()
 origVert = [x.rstrip() for x in origVert]
 semanticTree = [x.rstrip() for x in semanticTree]
 dictFormingSemanticTree = semanticTree
@@ -63,35 +68,39 @@ edgeDictionary = {}
 inboundPaths = {}
 #Todo: set pairings to be the new fangled dictionary thingamabobber
 pairings = []
+print("Making the three dictionaries now!")
 for line in dictFormingSemanticTree:
     elements = line.split(" ")
     edgeDictionary[elements[0]] = elements[1:] #Edge ID as key,  [src, dest, parent, weight] as vals
     #if both src and dst are in orig Vertices:
-    if(elements[1] in vertices.keys() and elements[2] in vertices.keys()):
+    if(elements[1] in vertices and elements[2] in vertices):
         #todo: reverify this makes sense. elements[1] is src, elements[2] is dest
         pairings.append((elements[1], elements[2]))
         if len(elements) == 4: #here there's EdgeID, src, dest, weight, here we ARE at the parent
-            if elements[1] in outboundPaths.keys():
+            if elements[1] in outboundPaths:
                 outboundPaths[elements[1]].update({elements[2]: [elements[0], elements[3]]})
             else:
-                outboundPaths[1] = {elements[2]: [elements[0], elements[3]]}
-            if elements[2] in inboundPaths.keys():
+                outboundPaths[elements[1]] = {elements[2]: [elements[0], elements[3]]}
+            if elements[2] in inboundPaths:
                 inboundPaths[elements[2]].update({elements[1]:[elements[0], elements[3]]})
             else:
                 inboundPaths[elements[2]] = {elements[1]:[elements[0], elements[3]]}
         elif len(elements) == 5: #here we are still at a child and have to append the parent. format: Src key -> dest Key -> [EdgeId, Weight, Parent if exists], if path[src][dest].size = 3, we gotta keep going, if =2, we at end
-            if elements[1] in outboundPaths.keys():
+            if elements[1] in outboundPaths:
                 outboundPaths[elements[1]].update({elements[2]: [elements[0], elements[4], elements[3]]})
             else:
-                outboundPaths[1] = {elements[2]: [elements[0], elements[4], elements[3]]}
-            if elements[2] in inboundPaths.keys():
+                outboundPaths[elements[1]] = {elements[2]: [elements[0], elements[4], elements[3]]}
+            if elements[2] in inboundPaths:
                 inboundPaths[elements[2]].update({elements[1]: [elements[0], elements[4], elements[3]]})
             else:
                 inboundPaths[elements[2]] = {elements[1]: [elements[0], elements[4], elements[3]]}
 
 
 print("Done setting up stuff, now pairing and pathing")
-print(getViewPortPaths(-5.5, -5, -5.5, -5, vertices, outboundPaths,inboundPaths, edgeDictionary))
+paths = (getViewPortPaths(-40, 40, -40, 40, vertices, outboundPaths,inboundPaths, edgeDictionary))
+print(len(paths))
 print("All roads generated. Have a great day! (hehe xd)")
-
+print(paths[12])
+newtime = datetime.datetime.now()
+print("Time elapsed: " + str(newtime-oldTime))
 #New implementation preferred: dict[src:dict[dest:[edgeId, weight, ParentID]]] BUT how do we find inbound edges quickly? do we make a reversed one?
