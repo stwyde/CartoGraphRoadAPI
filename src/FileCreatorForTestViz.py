c8ebd0a3733e098c5847a53128c5e187971913e9
Eloa Franca Verona <efrancav@macalester.edu>
2017-06-22 13:48:03 -0500

    file creator for test kind of working

---
+++ src/FileCreatorForTestViz.py
@@ -0,0 +1,27 @@
+import csv
+class FileCreator():
+    def generateFilesFromSourceDest(self, edgesList, verticeDic, bundledVerticeList, pointsInPort):
+        verticeOutput = open("./verticeForTest.txt", 'w')
+        verticeOutputWriter = csv.writer(verticeOutput, delimiter = ' ')
+
+        edgeOutput = open("./edgesForTest.txt", 'w')
+        edgeOutputWriter = csv.writer(edgeOutput, delimiter = ' ')
+        print("Starting here")
+        for edge_info in edgesList.heap:
+            weight = edge_info[0]
+            src = edge_info[1][0]
+            dest = edge_info[1][1]
+            if src in verticeDic:
+                verticeOutputWriter.writerow([src, verticeDic[src][0], verticeDic[src][2]])
+            elif src in bundledVerticeList:
+                verticeOutputWriter.writerow([src, bundledVerticeList[src][0], bundledVerticeList[src][1]])
+            if dest in verticeDic:
+                verticeOutputWriter.writerow([dest, verticeDic[dest][0], verticeDic[dest][1]])
+            elif dest in bundledVerticeList:
+                verticeOutputWriter.writerow([dest, bundledVerticeList[dest][0], bundledVerticeList[dest][1]])
+            edgeOutputWriter.writerow([src, dest, weight])
+        for point in pointsInPort:
+            verticeOutputWriter.writerow([point, verticeDic[point][0], verticeDic[point][1]])
+        print("Done Here")
+        verticeOutput.close()
+        edgeOutput.close()
